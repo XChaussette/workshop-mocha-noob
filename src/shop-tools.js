d43ef41d25f05ef412a5c814c4dcdc3d087c24be
bastien coadou <bastien.coadou@yahoo.fr>
2017-12-12 15:28:25 +0100

    correctif exo 2

--- src/shop-tools.js
+++ src/shop-tools.js
@@ -1,3 +1,7 @@
 exports.getPrices=function(){
     return{banana: 1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6}
 }
+exports.countArticles=function(){
+    const products=['tomato','cucumber', 'tomato', 'salad', 'potato', 'cucumber', 'potato', 'potato', 'tomato', 'potato']
+    return products.length+1;
+}
--- test/shop-tools.test.js
+++ test/shop-tools.test.js
@@ -1,5 +1,6 @@
 var expect=require('chai').expect;
 var getPrices=require('../src/shop-tools').getPrices;
+var countArticles=require('../src/shop-tools').countArticles();
 describe('A function +getPrices+ that doesn\'t take parameter should return an object which describes the prices of your products.',function(){
     it('should be true',function(){
         expect(true).to.be.true;
@@ -8,5 +9,10 @@ describe('A function +getPrices+ that doesn\'t take parameter should return an o
 describe('function getPrices()',function(){
     it('expect return the good results',function(){
         expect(getPrices()).to.eql({banana: 1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6 });
+    })
+})
+describe('function countArticle()',function(){
+    it('we expect the good result',function(){
+        
     })
 })
