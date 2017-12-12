2bb0d45e23f0a3ed47e314aa2415a64d2175feed
bastien coadou <bastien.coadou@yahoo.fr>
2017-12-12 15:19:43 +0100

    ajout de l'exercice 1 fonctionnel

---
+++ src/Exercise 1.js
@@ -0,0 +1,7 @@
+const tab=['banana : 1','potato 2','tomato 3','cucumber 4','salad 5','apple 6'];
+function getPrice(){
+    for(i=0;i<tab.length;i++)
+    {
+        console.log(tab[i]);
+    }
+}
---
+++ test/shop-tools.test.js
@@ -0,0 +1,12 @@
+var expect=require('chai').expect;
+var getPrices=require('../src/shop-tools').getPrices;
+describe('A function +getPrices+ that doesn\'t take parameter should return an object which describes the prices of your products.',function(){
+    it('should be true',function(){
+        expect(true).to.be.true;
+    })
+})
+describe('function getPrices()',function(){
+    it('expect return the good results',function(){
+        expect(getPrices()).to.eql({banana: 1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6 });
+    })
+})
